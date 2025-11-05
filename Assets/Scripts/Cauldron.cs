1c07748728aee39421d88fbfc32181740aff344b
mikael-gr <157851063+mikael-gr@users.noreply.github.com>
2025-11-04 16:29:18 -0800

    Part 4.3: Cauldron VFX

--- Assets/Scripts/Cauldron.cs
+++ Assets/Scripts/Cauldron.cs
@@ -6,16 +6,18 @@ using System.Linq;
 public class Cauldron : MonoBehaviour
 {
     public GameObject player;
+    public GameObject menu;
     public List<int> contents;
+    public List<ObjectScript> theContents;
+    public List<GameObject> inCauldron;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
     }
     
@@ -31,5 +33,28 @@ public class Cauldron : MonoBehaviour
 
     public void AddToCauldron(int id) {
         contents.Add(id);
+    }
+
+    public void AddToList(ObjectScript obj) {
+        theContents.Add(obj);
+    }
+
+    public void CauldronInsert(GameObject obj) {
+        inCauldron.Add(obj);
+    }
+
+    public bool CheckListForDuplicates(int id) {
+        foreach (GameObject tempObject in inCauldron) {
+            if (tempObject.GetComponent<ObjectScript>() != null)
+                if (tempObject.GetComponent<ObjectScript>().GetObjectID() == id)
+                    return true;
+        } return false;
+    }
+
+    public void UnloadContents(int id) {
+        foreach (GameObject tempObject in inCauldron) {
+            tempObject.SetActive(true);
+        }
+        inCauldron.Clear();
     }
 }
--- Assets/Scripts/ObjectScripts/ObjectScript.cs
+++ Assets/Scripts/ObjectScripts/ObjectScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class ObjectScript : MonoBehaviour {
     [SerializeField] int id = 0;
     [SerializeField] string objectName = "";
+    [SerializeField] private GameObject cauldronAnimation;
+    [SerializeField] private Transform cauldronTransform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         
@@ -19,13 +21,18 @@ public class ObjectScript : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Cauldron"))
-        {
-            Destroy(gameObject);
-            other.GetComponent<Cauldron>().AddToCauldron(id);
+        if (other.CompareTag("Cauldron")) {
+            // If id is not in list, add the id to the list.
+            // other.GetComponent<Cauldron>().AddToCauldron(id);
+            // other.GetComponent<Cauldron>().AddToList(GetComponent<ObjectScript>());
+            if (!other.GetComponent<Cauldron>().CheckListForDuplicates(id)) {
+                gameObject.SetActive(false);
+                other.GetComponent<Cauldron>().CauldronInsert(gameObject);
+                GameObject particle = Instantiate(cauldronAnimation, cauldronTransform.position, cauldronTransform.rotation);
+                Destroy(particle, 2);
+            }
         }
-        if (other.CompareTag("Player"))
-        {
+        if (other.CompareTag("Player")) {
             Debug.Log("Object colliding with Player");
         }
     }
