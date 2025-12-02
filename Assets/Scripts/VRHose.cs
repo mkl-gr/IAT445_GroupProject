c57282371b820dd0dd6771dff876a19e055b1237
mikael-gr <157851063+mikael-gr@users.noreply.github.com>
2025-12-01 18:12:25 -0800

    Part 9.1: Post-last meeting

--- Assets/Scripts/VRHose.cs
+++ Assets/Scripts/VRHose.cs
@@ -7,6 +7,10 @@ public class VRHose : MonoBehaviour
     public OVRInput.Button sprayInput;
     public OVRInput.Button shrinkInput;
     public OVRInput.Button enlargeInput;
+    public OVRInput.Button changeWeaponInput;
+    int sprayMode = 0;
+    [SerializeField] public int changeModeCooldown = 60;
+    private int currChangeModeCooldown = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -14,8 +18,19 @@ public class VRHose : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        if (currChangeModeCooldown > 0) {
+            currChangeModeCooldown--;
+        }
+
+        if ( (OVRInput.Get(changeWeaponInput) || Input.GetKeyDown(KeyCode.O)) && currChangeModeCooldown == 0) {
+            if (sprayMode == 0 ) sprayMode = 1;
+            else if (sprayMode == 1 ) sprayMode = 2;
+            else if (sprayMode == 2 ) sprayMode = 0;
+            currChangeModeCooldown += changeModeCooldown;
+        }
+
         //original script
-        if (OVRInput.Get(sprayInput) || Input.GetKeyDown(KeyCode.U))
+        if ((OVRInput.Get(sprayInput) || Input.GetKeyDown(KeyCode.U)) && sprayMode == 0)
         {
             Debug.Log("SHOOT!");
             theHose.StartSpray();
@@ -24,7 +39,7 @@ public class VRHose : MonoBehaviour
         //check if the shrink input was pressed
         //makes it so that the shrink projectile is shot (called from SprayHose script)
         //using keyboard for testing purposes, want to add an input for vr specifically
-        if (OVRInput.Get(shrinkInput) || Input.GetKeyDown(KeyCode.O))
+        if ((OVRInput.Get(sprayInput) || Input.GetKeyDown(KeyCode.U)) && sprayMode == 1)
         {
             Debug.Log("SHOOT!");
             theHose.StartSpray("shrink");
@@ -33,7 +48,7 @@ public class VRHose : MonoBehaviour
         //check if the grow input was pressed
         //makes it so that the grow projectile is shot (called from SprayHose script)
         //using keyboard for testing purposes, want to add an input for vr specifically
-        if (OVRInput.Get(enlargeInput) || Input.GetKeyDown(KeyCode.P))
+        if ((OVRInput.Get(sprayInput) || Input.GetKeyDown(KeyCode.U)) && sprayMode == 2)
         {
             Debug.Log("SHOOT!");
             theHose.StartSpray("grow");
